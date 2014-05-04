f4d066ecf77711d40169d4c46260693df7505125
Mateusz Czajka <mateusz.czajka@netguru.pl>
2014-05-04 21:50:09 +0200

    add user photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User
   field :name, type: String, default: ''
   field :oauth_token, type: String
   field :oauth_expires_at, type: DateTime
+  field :image_url, type: String
 
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
@@ -17,8 +18,13 @@ class User
       user.name = auth.info.name
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.image_url = auth.info.image
       user.save!
     end
+  end
+
+  def photo size = 100
+    "#{image_url}?height=#{size}&width=#{size}"
   end
 
 end
